97d427570be0796714ffced0145bd612c7e43578
Niemand-co <735232844@qq.com>
2022-11-07 18:31:14 +0800

    [Temporal]Editor

--- Engine/Editor/Private/Editor.cpp
+++ Engine/Editor/Private/Editor.cpp
@@ -1,7 +1,36 @@
 #include "pch.h"
 #include "Editor/Public/Editor.h"
+#include "Scene/Public/GameObject.h"
 
 namespace WEngine
 {
+
+	std::vector<GameObject*> Editor::g_selectedObjects = std::vector<GameObject*>();
+
+	Editor::Editor()
+	{
+	}
+
+	void Editor::SelectObject(GameObject* pGameObject)
+	{
+		g_selectedObjects.clear();
+		g_selectedObjects.push_back(std::move(pGameObject));
+	}
+
+	void Editor::SelectObject(const std::vector<GameObject*>& gameObjects)
+	{
+		g_selectedObjects.clear();
+		g_selectedObjects = gameObjects;
+	}
+
+	std::vector<GameObject*>& Editor::GetSelectedObject()
+	{
+		return g_selectedObjects;
+	}
+
+	size_t Editor::GetSelectedObjectCount()
+	{
+		return g_selectedObjects.size();
+	}
 
 }
--- Engine/Editor/Public/Editor.h
+++ Engine/Editor/Public/Editor.h
@@ -1,10 +1,29 @@
 #pragma once
 
+class GameObject;
+
 namespace WEngine
 {
 
 	class Editor
 	{
+	public:
+
+		Editor();
+
+		~Editor() = default;
+
+		static void SelectObject(GameObject *pGameObject);
+
+		static void SelectObject(const std::vector<GameObject*>& gameObjects);
+
+		static std::vector<GameObject*>& GetSelectedObject();
+
+		static size_t GetSelectedObjectCount();
+
+	public:
+
+		static std::vector<GameObject*> g_selectedObjects;
 
 	};
 
